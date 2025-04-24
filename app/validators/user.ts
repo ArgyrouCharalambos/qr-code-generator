ee4264a3202a0297620cebe841e261b1c7c6f3fa
Argyrou <argykaselve@gmail.com>
2025-04-24 12:42:03 +0200

    Ajout des différent validateur pour la connexion ,l'inscription et le mot de passe oublié

--- app/validators/user.ts
+++ app/validators/user.ts
@@ -1,1 +1,27 @@
 import vine from '@vinejs/vine'
+
+export const createUserValidator = vine.compile(
+    vine.object({
+        fullName: vine.string().maxLength(25),
+        email: vine.string().activeUrl(),
+        password: vine.string().minLength(8)
+    })
+)
+
+export const connexionUserValidator = vine.compile(
+    vine.object({
+        email: vine.string().activeUrl(),
+        password: vine.string().minLength(8)
+    })
+)
+
+export const forgotPasswordValidator = vine.compile(
+    vine.object({
+        email: vine.string().activeUrl(),
+    })
+)
+export const changePasswordValidator = vine.compile(
+    vine.object({
+        password: vine.string().minLength(8)
+    })
+)
