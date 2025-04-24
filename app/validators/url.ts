95329534b4571ce4ec2bba9896447483581621ed
Argyrou <argykaselve@gmail.com>
2025-04-24 11:38:36 +0200

    création de validateur pour la création et la modification d'un lien

--- app/validators/url.ts
+++ app/validators/url.ts
@@ -1,1 +1,14 @@
 import vine from '@vinejs/vine'
+
+export const createUrlValidator = vine.compile(
+    vine.object({
+      lien: vine.string().email(),
+    })
+  )
+
+  export const updateUrlValidator = vine.compile(
+    vine.object({
+      lien: vine.string().email(),
+      code: vine.number()
+    })
+  )
